adc298a9a0e57391cbd9646197294845928c38eb
gabrieldiaspereira <gabrieldiaspmg@gmail.com>
2023-08-15 19:07:29 -0300

    feat: add request and response helpers

---
+++ handler/request.go
@@ -0,0 +1,43 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+// CreateOpening
+
+type CreateOpeningRequest struct {
+	Role     string `string:"role"`
+	Company  string `string:"company"`
+	Location string `string:"location"`
+	Remote   *bool  `string:"remote"`
+	Link     string `string:"link"`
+	Salary   int64  `string:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Link == "" && r.Remote == nil && r.Salary <= 0 {
+		return fmt.Errorf("request body is empty or malformed")
+	}
+	if r.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+	if r.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+	if r.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+	if r.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+	if r.Salary <= 0 {
+		return errParamIsRequired("salary", "int64")
+	}
+	return nil
+}
